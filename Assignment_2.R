cce80c46c71c89aa556963f0f935441c2c1263fb
jic102 <chenjie05046@gmail.com>
2017-02-05 19:21:38 -0800

    Update Assignment_2.R

--- Assignment_2.R
+++ Assignment_2.R
@@ -1,7 +1,7 @@
 
 ## makeCacheMatrix: This function creates a special "matrix" object 
 ## that can cache its inverse.
-makeCacheMatrix <- function(x = numeric()) {
+makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
